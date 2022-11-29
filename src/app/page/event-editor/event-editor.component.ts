8f26edff9977c5a571eae2194e8fcf3a7751cc3c
Anankaaa <hegyaljai83@hotmail.com>
2022-11-29 11:24:59 +0100

    create working

--- src/app/page/event-editor/event-editor.component.ts
+++ src/app/page/event-editor/event-editor.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { EventService } from 'src/app/service/event.service';
 import { Event } from 'src/app/model/event';
@@ -15,8 +15,11 @@ export class EventEditorComponent implements OnInit {
   // 1. Kiolvasni az id paramétert az URL-ből.
   // 2. Ezzel a paraméterrel meghívni az EventService.get metódust.
   event$: Observable<Event> = this.activatedRoute.params.pipe(
-    switchMap((params) => this.eventService.get(params['id']))
+    switchMap((params) =>
+    params['id'] != 0 ? this.eventService.get(params['id']): of(new Event()))
   );
+
+
 
   event: Event = new Event;
 
@@ -28,10 +31,11 @@ export class EventEditorComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  onUpdate(form: NgForm, event: Event): void {
+  onUpdate(eventForm: NgForm, event: Event): void {
    if(event.id === 0) {
     this.eventService.create(event).subscribe(event => this.router.navigate(['/', 'event']))
    }
     this.eventService.update(event).subscribe((event) => this.router.navigate(['/', 'event']));
   }
+
 }
